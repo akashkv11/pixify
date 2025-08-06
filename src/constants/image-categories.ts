d8515f25fe4ee9d9f01e929d49e5d2f2da0ef66b
akashk11 <akashvinod777@gmail.com>
2025-08-06 23:15:51 +0400

    feat: add image categories for SFW and NSFW content

---
+++ src/constants/image-categories.ts
@@ -0,0 +1,50 @@
+const sfwCategories = [
+  "waifu",
+  "neko",
+  "shinobu",
+  "megumin",
+  "bully",
+  "cuddle",
+  "cry",
+  "hug",
+  "awoo",
+  "kiss",
+  "lick",
+  "pat",
+  "smug",
+  "bonk",
+  "yeet",
+  "blush",
+  "smile",
+  "wave",
+  "highfive",
+  "handhold",
+  "nom",
+  "bite",
+  "glomp",
+  "slap",
+  "kill",
+  "kick",
+  "happy",
+  "wink",
+  "poke",
+  "dance",
+  "cringe",
+] as const;
+
+const nsfwCategories = ["waifu", "neko", "trap", "blowjob"] as const;
+
+const versatile = [
+  "maid",
+  "waifu",
+  "marin-kitagawa",
+  "mori-calliope",
+  "raiden-shogun",
+  "oppai",
+  "selfies",
+  "uniform",
+  "kamisato-ayaka",
+] as const;
+const nsfw = ["ass", "hentai", "milf", "oral", "paizuri", "ecchi", "ero"] as const;
+
+export { sfwCategories, nsfwCategories, versatile, nsfw };
